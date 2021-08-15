4578cd9908b0a06a24084561a59601b8b9b84460
David Dollar <david@dollar.io>
2021-08-14 21:13:09 -0400

    branch: terraform-1.0 ([diff](https://github.com/ddollar/convox/compare/master...terraform-1.0))

--- terraform/resolver/k8s/main.tf
+++ terraform/resolver/k8s/main.tf
@@ -160,7 +160,7 @@ resource "kubernetes_deployment" "resolver" {
           }
 
           resources {
-            requests {
+            requests = {
               cpu    = "64m"
               memory = "64Mi"
             }
