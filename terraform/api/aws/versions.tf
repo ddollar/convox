40f9c070e35b4514286920781f3f50fdcee82eaf
David Dollar <david@dollar.io>
2021-08-15 09:46:58 -0400

    branch: aws-loki ([diff](https://github.com/ddollar/convox/compare/master...aws-loki))

--- terraform/api/aws/versions.tf
+++ terraform/api/aws/versions.tf
@@ -4,6 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 2.49"
     }
+    helm = {
+      source = "hashicorp/helm"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 1.11"
